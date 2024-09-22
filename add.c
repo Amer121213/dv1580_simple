521aa58b9e36151618c9ee0166d495594d0459e8
Amer <47393854+Amer121213@users.noreply.github.com>
2024-09-22 22:46:30 +0200

    Update add.c

--- add.c
+++ add.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("Usage: %s num1 num2\n", argv[0]);
+        return 1;
+    }
+    
+    int a1 = atoi(argv[1]);
+    int a2 = atoi(argv[2]);
+    printf("%d\n", a1 + a2);
 
-  return 0;
+    return 0;
 }
