9fffa0092f19bc5d57d9f878858a3d64caa92f5c
Amer <47393854+Amer121213@users.noreply.github.com>
2024-09-22 22:47:06 +0200

    Update mult.c

--- mult.c
+++ mult.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-int main(int argc, char *argv[]){
+int isNumber(char *str) {
+    int dotCount = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '.') {
+            dotCount++;
+        } else if (!isdigit(str[i])) {
+            return 0;
+        }
+    }
+    return dotCount <= 1;  // Only one decimal point allowed
+}
 
-  return 0;
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
+        printf("Usage: %s num1 num2\n", argv[0]);
+        return 1;
+    }
+
+    if (!isNumber(argv[1]) || !isNumber(argv[2])) {
+        printf("Invalid input\n");
+        return 1;
+    }
+
+    double a1 = atof(argv[1]);
+    double a2 = atof(argv[2]);
+    printf("%.4f\n", a1 * a2);
+
+    return 0;
 }
